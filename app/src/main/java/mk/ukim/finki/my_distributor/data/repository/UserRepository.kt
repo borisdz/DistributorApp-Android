95a0c6d1bc7acc763e4ae3fc96ba06a4e317ee61
Boris Djotov <borisdzotov@gmail.com>
2025-02-23 07:25:46 +0100

    API connection and initial user repository, service, view model and DTO.

---
+++ app/src/main/java/mk/ukim/finki/my_distributor/data/repository/UserRepository.kt
@@ -0,0 +1,19 @@
+package mk.ukim.finki.my_distributor.data.repository
+
+import mk.ukim.finki.my_distributor.data.api.UserApiService
+import mk.ukim.finki.my_distributor.domain.UserDto
+
+class UserRepository(private val userApiService: UserApiService) {
+    suspend fun getUserByEmail(email: String): Result<UserDto> {
+        return try{
+            val response = userApiService.getUserByEmail(email)
+            if(response.isSuccessful && response.body()!=null){
+                Result.success(response.body()!!)
+            }else{
+                Result.failure(Exception("Fetching User data failed: ${response.code()}"))
+            }
+        }catch (e: Exception){
+            Result.failure(e)
+        }
+    }
+}
