21bb32b7749ad33fb96abace9819ecb8cb860332
Boris Djotov <borisdzotov@gmail.com>
2025-03-03 18:34:32 +0100

    Added the other necessary fragments. Need to further implement their functionalities.

--- app/src/main/java/mk/ukim/finki/my_distributor/ui/activities/CustomerActivity.kt
+++ app/src/main/java/mk/ukim/finki/my_distributor/ui/activities/CustomerActivity.kt
@@ -3,10 +3,12 @@ package mk.ukim.finki.my_distributor.ui.activities
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import mk.ukim.finki.my_distributor.R
 import mk.ukim.finki.my_distributor.databinding.ActivityCustomerBinding
+import mk.ukim.finki.my_distributor.ui.fragments.customer.CreateOrderFragment
+import mk.ukim.finki.my_distributor.ui.fragments.customer.CustomerDashboardFragment
+import mk.ukim.finki.my_distributor.ui.fragments.customer.ProfileFragment
 
 class CustomerActivity : AppCompatActivity() {
 
@@ -20,5 +22,24 @@ class CustomerActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         // TODO: Implement customer activity logic
+
+        val bottomNav = findViewById<BottomNavigationView>(binding.bottomNavigation.id)
+
+        bottomNav.setOnItemSelectedListener { item->
+            val selectedFragment = when (item.itemId) {
+                R.id.nav_dashboard -> CustomerDashboardFragment()
+                R.id.nav_create_order -> CreateOrderFragment()
+                R.id.nav_profile -> ProfileFragment()
+                else -> CustomerDashboardFragment()
+            }
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container,selectedFragment)
+                .commit()
+            true
+        }
+
+        if (savedInstanceState == null){
+            bottomNav.selectedItemId = R.id.nav_dashboard
+        }
     }
 }
